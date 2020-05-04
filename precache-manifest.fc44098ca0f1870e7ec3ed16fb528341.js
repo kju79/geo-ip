c25508abcc706acc5a25580c02791c5b946bede2
René Wildförster <rwgalaxy3@gmail.com>
2020-05-04 22:45:39 +0200

    Updates

---
+++ precache-manifest.fc44098ca0f1870e7ec3ed16fb528341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a568de039c6cb8787b7251190d6669",
+    "url": "/geo-ip/index.html"
+  },
+  {
+    "revision": "b141908992d1232a72a3",
+    "url": "/geo-ip/static/css/2.6f3a26c5.chunk.css"
+  },
+  {
+    "revision": "ed680947377bb3495ee7",
+    "url": "/geo-ip/static/css/main.ad9cc6b0.chunk.css"
+  },
+  {
+    "revision": "b141908992d1232a72a3",
+    "url": "/geo-ip/static/js/2.0e274d36.chunk.js"
+  },
+  {
+    "revision": "77ad6023570002ac00e6fc11e8c0c904",
+    "url": "/geo-ip/static/js/2.0e274d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed680947377bb3495ee7",
+    "url": "/geo-ip/static/js/main.4dad4a20.chunk.js"
+  },
+  {
+    "revision": "aadc4f9f772e28821251",
+    "url": "/geo-ip/static/js/runtime-main.27aab6cf.js"
+  }
+]);
